1c0f4694a5e3e31b7f57a2d74741ece1cfb140bf
giulianoh92 <giulianohillebrand@gmail.com>
2024-11-06 09:03:51 -0300

    feat(examples): añadir ejemplo de lectura de archivos usando async/await

---
+++ 4.fs-async-await.js
@@ -0,0 +1,16 @@
+const {readFile} = require('node:fs/promises');
+
+(
+    async () => {
+        console.log('Leyendo el primer archivo...');
+        const text = await readFile('./file.txt', 'utf-8');
+        console.log(text);
+
+        console.log('Hace algo más...');
+
+        console.log('Leyendo el segundo archivo...');
+        const text2 = await readFile('./file2.txt', 'utf-8');
+        console.log(text2);
+    }
+)();
+
