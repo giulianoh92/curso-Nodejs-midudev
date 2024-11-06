88a00b0fb97611e16659cf2eefcdd78d335b40fd
giulianoh92 <giulianohillebrand@gmail.com>
2024-11-06 12:09:24 -0300

    feat(path): agregar ejemplo de uso del módulo path para manipulación de rutas de archivos

---
+++ 5.path.js
@@ -0,0 +1,14 @@
+const path = require('node:path');
+
+
+const filePath = path.join('./content', 'subfolder', 'test.txt');
+console.log(filePath);
+
+const base = path.basename(filePath);
+console.log(base);
+
+const filename = path.basename(filePath, '.txt');
+console.log(filename);
+
+const extension = path.extname(filePath);
+console.log(extension);
