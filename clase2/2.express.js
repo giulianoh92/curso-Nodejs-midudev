035af0eca353e9a85e4254de1113325586f0eec4
giulianoh92 <giulianohillebrand@gmail.com>
2024-11-10 17:02:31 -0300

    feat(express-server): agregar middleware para manejo de datos JSON y actualizar rutas API

--- clase2/2.express.js
+++ clase2/2.express.js
@@ -5,12 +5,15 @@ const PORT = process.env.PORT ?? 3000; //definir el puerto
 const app = express(); //crear una instancia de express
 app.disable('x-powered-by'); //desactivar la cabecera X-Powered-By
 
+app.use(express.json()); //definir un middleware
 
-app.get('/', (req, res) => { //definir una ruta
-    res.status(200).send('Hola Mundo'); //enviar una respuesta
-});
+/*
+app.use('/api/*', (req, res, next) => { //definir un middleware
+    if(req.method != 'POST') return next(); //si el método no es POST, continuar
+    if(req.headers['content-type'] != 'application/json') return next(); //si el content-type no es application/json, continuar
 
-app.post('/api/user', (req, res) => { //definir una ruta
+    //solo llegan request POST con content-type application/json
+
     let body = '';
     req.on('data', (chunk) => { //escuchar los datos que se envían
         body += chunk.toString(); //concatenar los datos
@@ -18,12 +21,25 @@ app.post('/api/user', (req, res) => { //definir una ruta
 
     req.on('end', () => { //escuchar el evento end
         const data = JSON.parse(body); //convertir los datos a JSON
-        res.status(201).json(data); //enviar una respuesta
+        data.timestamp = Date.now(); //agregar una propiedad timestamp
+        req.body = data; //asignar los datos al body de la request
+        next(); //continuar
     });
+});
+*/
+
+app.get('/', (req, res) => { //definir una ruta
+    res.status(200).send('Hola Mundo'); //enviar una respuesta
+});
+
+app.post('/api/data', (req, res) => { //definir una ruta
+    res.status(201).json(req.body); //enviar una respuesta
 });
 
 app.use((req, res) => { //definir un middleware
     res.status(404).send('Página no encontrada'); //enviar una respuesta
+    //por que el ultimo middleware no tiene next()?
+    //porque no hay más middleware después de este
 });
 
 app.listen(PORT, () => { //iniciar el servidor
