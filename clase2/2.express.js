ba84b55c64ed5899e58d23dd98ae8ad21a701c53
giulianoh92 <giulianohillebrand@gmail.com>
2024-11-10 16:33:28 -0300

    feat(express-server): agregar rutas GET y POST, middleware para manejo de errores y desactivar cabecera X-Powered-By

--- clase2/2.express.js
+++ clase2/2.express.js
@@ -1,10 +1,29 @@
 const express = require('express'); //importar express
-const app = express(); //crear una instancia de express
 
 const PORT = process.env.PORT ?? 3000; //definir el puerto
 
+const app = express(); //crear una instancia de express
+app.disable('x-powered-by'); //desactivar la cabecera X-Powered-By
+
+
 app.get('/', (req, res) => { //definir una ruta
     res.status(200).send('Hola Mundo'); //enviar una respuesta
+});
+
+app.post('/api/user', (req, res) => { //definir una ruta
+    let body = '';
+    req.on('data', (chunk) => { //escuchar los datos que se envían
+        body += chunk.toString(); //concatenar los datos
+    });
+
+    req.on('end', () => { //escuchar el evento end
+        const data = JSON.parse(body); //convertir los datos a JSON
+        res.status(201).json(data); //enviar una respuesta
+    });
+});
+
+app.use((req, res) => { //definir un middleware
+    res.status(404).send('Página no encontrada'); //enviar una respuesta
 });
 
 app.listen(PORT, () => { //iniciar el servidor
