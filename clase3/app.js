5bc37262cbfc470d2cf19b6f4913527d6dc173ca
giulianoh92 <giulianohillebrand@gmail.com>
2024-11-10 21:34:49 -0300

    feat(express-server): implementar rutas GET, POST y PATCH para manejo de películas, incluyendo validación y filtrado

--- clase3/app.js
+++ clase3/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const fs = require('node:fs');
+const crypto = require('node:crypto');
 const movies = require('./movies.json');
+const { validateMovie, validatePartialMovie } = require('./schemas/movies');
 
 const app = express();
 app.disable('x-powered-by'); // Deshabilita la cabecera X-Powered-By
@@ -12,7 +15,71 @@ app.get('/', (req, res) => {
 });
 
 app.get('/movies', (req, res) => {
+    const { genre } = req.query;
+    if (genre) {
+        const filteredMovies = movies.filter(
+            movie => movie.genre.toLowerCase() === genre.toLowerCase()
+        );
+        return res.json(filteredMovies);
+    }
     res.json(movies);
+});
+
+app.get('/movies/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const movie = movies.find(movie => movie.id === id);
+
+    if (!movie) {
+        return res.status(404).json({ message: 'Movie not found' });
+    }
+
+    res.json(movie);
+});
+
+app.post('/movies', (req, res) => {
+    const moviesArray = Array.isArray(req.body) ? req.body : [req.body];
+    const newMovies = [];
+
+    for (const movieData of moviesArray) {
+        const result = validateMovie(movieData);
+
+        if (result.error) {
+            return res.status(422).json({ message: result.error.message });
+        }
+
+        const newMovie = {
+            id: crypto.randomUUID(),
+            ...movieData
+        };
+
+        movies.push(newMovie);
+        newMovies.push(newMovie);
+    }
+
+    fs.writeFileSync('./movies.json', JSON.stringify(movies, null, 2));
+
+    res.status(201).json(newMovies);
+});
+
+app.patch('/movies/:id', (req, res) => {
+    const { id } = req.params;
+    const movie = movies.find(movie => movie.id === id);
+
+    if (!movie) {
+        return res.status(404).json({ message: 'Movie not found' });
+    }
+
+    const result = validatePartialMovie(req.body);
+
+    if (result.error) {
+        return res.status(422).json({ message: result.error.message });
+    }
+
+    Object.assign(movie, req.body);
+
+    fs.writeFileSync('./movies.json', JSON.stringify(movies, null, 2));
+
+    res.json(movie);
 });
 
 app.listen(port, () => {
